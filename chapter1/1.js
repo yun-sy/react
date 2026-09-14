b994038c091296c2b7aca9980ad974d9e2404cc2
yunsy <yunsy0119@gmail.com>
2026-09-14 20:35:53 +0900

    add 변수

---
+++ chapter1/1.js
@@ -0,0 +1,13 @@
+// 변수
+
+console.log("Hello");
+
+let age = 25;
+console.log(age);
+age = 30;
+console.log(age);
+
+let name1;
+let nickname = "winterlood";
+let user = name1 ?? nickname;
+console.log(user);
